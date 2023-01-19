e19e7155ad4520f1a60c0e3fc80581ef908996d6
Konrad_Krysiak <konradkrysiak17@gmail.com>
2023-01-19 22:07:39 +0100

    add mocha and chai libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
-  'env': {
+  env: {
     browser: true,
     es2021: true,
     node: true,
+    mocha: true,
   },
-  'extends': ['eslint:recommended'],
-  'overrides': [
-  ],
-  'parserOptions': {
-    ecmaVersion: 'latest',
+  extends: ["eslint:recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
   },
-  'plugins': ['babel'],
-  'parser': '@babel/eslint-parser',
-  'rules': {
-  },
-
+  plugins: ["babel"],
+  parser: "@babel/eslint-parser",
+  rules: {},
 };
