62513903a7bf3bfa57a4a196d2820ba0e1544e85
Konrad_Krysiak <konradkrysiak17@gmail.com>
2023-01-25 14:01:05 +0100

    add menu controller skeleton, routes and views

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -1,7 +1,92 @@
+import db from "../models";
+
+const { Restaurant, Menu } = db;
+
 class MenuController {
   // GET
-  index = (req, res) => {
-    res.render("menus/menus_index", { layout: "layouts/dashboard" });
+  indexView = async (req, res) => {
+    try {
+      const restaurantsWithMenus = await Restaurant.findAll({
+        where: { userId: req.user.id },
+        include: { model: Menu, required: true },
+      });
+      console.log(restaurantsWithMenus);
+      res.render("menus/menus_index", {
+        layout: "layouts/dashboard",
+        restaurantsWithMenus,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+      //
+    }
+  };
+
+  newView = async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll();
+      res.render("menus/menus_new", {
+        layout: "layouts/dashboard",
+        restaurants: restaurants.map((i) => ({
+          name: i.dataValues.name,
+          uuid: i.dataValues.uuid,
+        })),
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+      //
+    }
+  };
+
+  editView = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const menu = await Menu.findOne({ where: { id } });
+      res.render("menus/menus_edit", { layout: "layouts/dashboard", menu });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+      //
+    }
+  };
+
+  // SHOW VIEW COULD BE ELEGANT PDF PREVIEW
+  showView = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const menu = await Menu.findOne({ where: { id } });
+      res.render("menus/menus_show", { layout: "layouts/dashboard", menu });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+      //
+    }
+  };
+
+  create = async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findOne({
+        where: { uuid: req.body.restaurantUuid },
+      });
+      const menu = await restaurant.createMenu({ name: req.body.name });
+      res.render("menus/menus_edit", { layout: "layouts/dashboard", menu });
+    } catch (e) {
+      console.log(e);
+      //
+    }
+  };
+
+  delete = async (req, res) => {
+    try {
+      const id = req.params.id;
+      await Menu.destroy({ where: { id } });
+      res.redirect("back");
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+      //
+    }
   };
 }
 
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -14,6 +14,12 @@ router.post("/restaurants/new", RestaurantController.create);
 router.put("/restaurants/:id", RestaurantController.edit);
 router.delete("/restaurants/:id", RestaurantController.delete);
 
-router.get("/menus", MenuController.index);
+router.get("/menus", MenuController.indexView);
+router.get("/menus/new", MenuController.newView);
+router.get("/menus/:id", MenuController.showView);
+router.get("/menus/:id/edit", MenuController.editView);
+
+router.post("/menus/new", MenuController.create);
+router.delete("/menus/:id", MenuController.delete);
 
 export default router;
