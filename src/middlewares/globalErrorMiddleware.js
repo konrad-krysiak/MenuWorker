3108431d35622ba8b9499150761a5926732f7788
Konrad_Krysiak <konradkrysiak17@gmail.com>
2023-02-15 15:25:34 +0100

    customize winston logger, make modal product inputs requred

--- src/middlewares/globalErrorMiddleware.js
+++ src/middlewares/globalErrorMiddleware.js
@@ -1,7 +1,8 @@
 import { StatusCodes } from "http-status-codes";
+import winstonLogger from "../utils/logger";
 
 const globalErrorMiddleware = (err, req, res, next) => {
-  console.log("-------------- GLOBAL ERROR OCCURRED ", err);
+  winstonLogger.error("Global Error", err);
   res.status(err.status || StatusCodes.INTERNAL_SERVER_ERROR).json({
     error: err.message,
   });
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,25 +1,44 @@
-import winston from 'winston';
+import winston from "winston";
+
+// LOGGING LEVELS
+// {
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   http: 3,
+//   verbose: 4,
+//   debug: 5,
+//   silly: 6
+// }
+
+const logFormat = winston.format.printf(function (info) {
+  return `${info.level}: ${JSON.stringify(info.message, null, 4)}\n`;
+});
 
 const winstonLogger = winston.createLogger({
-  format: winston.format.combine(
-      winston.format.json(),
-      winston.format.prettyPrint(),
-  ),
   transports: [
     new winston.transports.File({
-      level: 'debug',
-      filename: './server.log',
+      level: "error",
+      filename: "./logs/server.log",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
       handleExceptions: true,
-      json: false,
-      maxsize: 5242880, // 5MB
-      colorize: false,
     }),
   ],
 });
-if (process.env.NODE_ENV !== 'production') {
-  winstonLogger.add(new winston.transports.Console({
-    format: winston.format.combine(winston.format.simple(), winston.format.colorize({ all: true })),
-  }));
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  winstonLogger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), logFormat),
+    })
+  );
 }
 
 export default winstonLogger;
