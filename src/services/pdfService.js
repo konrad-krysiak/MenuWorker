29adbb62745af295193dd7acfb7ccae7a9733c01
Konrad_Krysiak <konradkrysiak17@gmail.com>
2023-03-09 19:29:31 +0100

    fix(PDF): inject bootstrap css directly to generated pdf

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -1,19 +1,20 @@
-import "../bootstrap";
-
 import ejs from "ejs";
 import html_to_pdf from "html-pdf-node";
 import { resolve } from "path";
+import fs from "fs";
 
 import winstonLogger from "../utils/logger";
 
 class PdfService {
   #menuTemplateURL = resolve(process.env.MENU_TEMPLATE_URL);
+  #bootstrapUrl = resolve(process.env.BOOTSTRAP_CSS_URL);
 
   generateMenuPDF(menuEjsData, HtmlToPdfOptions = {}) {
+    const bootstrapFile = fs.readFileSync(this.#bootstrapUrl, "utf8");
     return new Promise((resolve, reject) => {
       ejs.renderFile(
         this.#menuTemplateURL,
-        { menu: menuEjsData },
+        { menu: menuEjsData, css: bootstrapFile },
         async (error, dataString) => {
           if (error) {
             winstonLogger.error("Error while generating pdf", error);
